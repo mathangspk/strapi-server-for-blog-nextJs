d9bafbefb37ff22f2d66d4f2c50de70ff42a411c
Ma Ngoc Hung Thang <mathangspk@gmail.com>
2021-03-01 10:55:55 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3dd355931b26676caa0048e29ec1be3f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c270b2a3-6bd3-4fc4-bb1a-034be4c7d603'
+};
